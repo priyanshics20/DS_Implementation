e65da331abd0a7e4ec4f3b8cd70a0bba8608e7a8
priyanshics20 <priyanshi.agrawal_cs20@gla.ac.in>
2022-03-10 21:23:30 +0530

    init

---
+++ src/Practice_Qus/BubbleSort_Qus1.java
@@ -0,0 +1,30 @@
+package Practice_Qus;
+
+import java.util.Scanner;
+
+public class BubbleSort_Qus1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String[] arr = str.split(" ");
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j].length()> arr[j+1].length()){
+                    String temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+                else if (arr[j].length() == arr[j + 1].length()){
+                    if (arr[j].compareTo(arr[j + 1])>0) {
+                        String temp = arr[j];
+                        arr[j]=arr[j+1];
+                        arr[j+1]=temp;
+                    }
+                }
+            }
+        }
+        for(int i=0;i< arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
