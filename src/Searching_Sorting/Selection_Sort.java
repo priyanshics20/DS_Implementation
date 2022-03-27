5ebc99a7123f2f8eb02482a3d2794467534f8922
priyanshics20 <priyanshi.agrawal_cs20@gla.ac.in>
2022-03-27 15:42:25 +0530

    init

--- src/Searching_Sorting/Selection_Sort.java
+++ src/Searching_Sorting/Selection_Sort.java
@@ -1,4 +1,35 @@
+/*
+Complexity is O(n^2)
+ */
 package Searching_Sorting;
 
+import java.util.Scanner;
+
 public class Selection_Sort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter array length");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i=0; i<n-1 ; i++) {
+            int smallest =i;
+            for(int j=i+1;j<n;j++){
+                if(arr[smallest]> arr[j]){
+                    smallest = j;
+                }
+            }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+
+        System.out.println("sorted array - ");
+        for (int i : arr){
+            System.out.print(i+" ");
+        }
+    }
 }
